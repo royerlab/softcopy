e20698b64f8c7bd7e3e248804b40cad7044efc4d
Seth Hinz <sethhinz@me.com>
2024-09-04 10:36:56 -0700

    Convert daxi zarr 2 acquisitions into ome zarr

--- src/softcopy/add_ome_metadata.py
+++ src/softcopy/add_ome_metadata.py
@@ -37,7 +37,12 @@ import json
 from pathlib import Path
 import itertools
 from shutil import move
+import sys
+import re
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
+import yaml
 import tensorstore as ts
 import click
 import numpy as np
@@ -45,10 +50,11 @@ import numpy as np
 from .zarr_copier import ZarrCopier
 
 # scope just for today: convert zarr 2 archives to ome zarr
+# dimension separator is '.' for this dataset
 
 @click.command()
 @click.argument("source", type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=Path))
-@click.argument("daxi-metadata", type=click.Path(exists=True, file_okay=True, dir_okay=False, path_type=Path), required=False)
+@click.argument("daxi-metadata", type=click.File(mode="r"), required=False)
 def main(source, daxi_metadata):
     zarr_json_path = ZarrCopier.find_zarr_json(source)
     zarr_version = None
@@ -65,14 +71,173 @@ def main(source, daxi_metadata):
         
         files_nd = np.ceil(shape / chunks).astype(int)
 
-    create_ome_zarr_folder_structure(source, files_nd)
-
+    # integrity check:
     for chunk in itertools.product(*[range(n) for n in files_nd]):
         chunk_file = source / ".".join(map(str, chunk))
         if not chunk_file.exists():
             print(f"Missing chunk file {chunk_file}")
-            break
-        move(chunk_file, source / "0" / Path(*map(str, chunk)))
+            exit(1)
+    
+    # Ensure we have all the metadata that we'll need:
+    if daxi_metadata:
+        dm = yaml.safe_load(daxi_metadata)
+        # print(dm)
+    
+    ome_metadata = {
+        "history": [
+            " ".join(sys.argv)
+        ],
+        "multiscales": [
+            {
+                "axes": [
+                    {
+                        "name": "T",
+                        "type": "time",
+                        "unit": "second"
+                    },
+                    {
+                        "name": "C",
+                        "type": "channel"
+                    },
+                    {
+                        "name": "Z",
+                        "type": "space",
+                        "unit": "micrometer"
+                    },
+                    {
+                        "name": "Y",
+                        "type": "space",
+                        "unit": "micrometer"
+                    },
+                    {
+                        "name": "X",
+                        "type": "space",
+                        "unit": "micrometer"
+                    }
+                ],
+                "coordinateTransformations": [
+                    {
+                        "type": "identity"
+                    }
+                ],
+                "datasets": [
+                    {
+                        "coordinateTransformations": [
+                            {
+                                "scale": [
+                                    1.0,
+                                    1.0,
+                                    dm["Z step size (um)"] if daxi_metadata else 1.24,
+                                    0.439,
+                                    0.439
+                                ],
+                                "type": "scale"
+                            }
+                        ],
+                        "path": "0"
+                    }
+                ],
+                "name": "0",
+                "version": "0.4"
+            }
+        ],
+        "omero": {
+            "channels": [
+                {
+                    "active": True,
+                    "coefficient": 1.0,
+                    "color": "FFFFFF",
+                    "family": "linear",
+                    "inverted": False,
+                    "label": "v0_c488",
+                    "window": {
+                        "end": 65535.0,
+                        "max": 65535.0,
+                        "min": 0.0,
+                        "start": 0.0
+                    }
+                },
+                {
+                    "active": True,
+                    "coefficient": 1.0,
+                    "color": "FFFFFF",
+                    "family": "linear",
+                    "inverted": False,
+                    "label": "v1_c488",
+                    "window": {
+                        "end": 65535.0,
+                        "max": 65535.0,
+                        "min": 0.0,
+                        "start": 0.0
+                    }
+                }
+            ],
+            "id": 0,
+            "name": "",
+            "rdefs": {
+                "defaultT": 0,
+                "defaultZ": 0,
+                "model": "color",
+                "projection": "normal"
+            },
+            "version": "0.4",
+        },
+        "daxi": {
+            "version": "0.0"
+        }
+    }
+
+    if daxi_metadata:
+        key_pattern = re.compile("T_(\d+).V_(\d+)")
+        tz = ZoneInfo('America/Los_Angeles')
+        time_windows = {}
+        # Collect all of the timestamps and append them to the ome-zarr metadata:
+        for (key, value) in dm.items():
+            match = key_pattern.match(key)
+            if match:
+                timepoint = int(match.group(1))
+                view = int(match.group(2))
+                start_dt, end_dt = [datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S").replace(tzinfo=tz) for date_str in (value["start"], value["end"])]
+                if timepoint not in time_windows:
+                    time_windows[timepoint] = {}
+                
+                time_windows[timepoint][view] = {
+                    "start": start_dt.isoformat(),
+                    "end": end_dt.isoformat()
+                }
+
+        ome_metadata["daxi"]["timing_detail"] = time_windows
+
+    zattrs = source / ".zattrs"
+    if zattrs.exists():
+        print(".zattrs file exists - aborting to avoid corruption.")
+        exit(1)
+    
+    with open(zattrs, "w") as zattrs_file:
+        zattrs_file.write(
+            json.dumps(ome_metadata, indent=4)
+        )
+    print(ome_metadata)
+    exit(1)
+    
+    # Create the folder for this multiscale:
+    (source / "0").mkdir(exist_ok=True)
+
+    # Execute the transformation:
+    # Move the .zarray file:
+    print(source / ".zarray", source / "0" / ".zarray")
+
+    for chunk in itertools.product(*[range(n) for n in files_nd]):
+        filename = ".".join(map(str, chunk))
+        chunk_file = source / filename
+        print(chunk_file, source / "0" / filename)
+
+        # Sheng's code uses a format given by Time x View x Color x Z x Y x X
+        # The acquisitions we use are only ever single color at the moment so we can probably just ignore
+        # that key
+        # move(chunk_file, source / "0" / Path(*map(str, chunk)))
+        # parts = chunk[:2] + chunk[3:]
+        # print(chunk_file, source / "0" / Path(*map(str, parts)))
 
 def create_ome_zarr_folder_structure(destination, files_nd):
     for coord in itertools.product(*[range(n) for n in files_nd[:-1]]):
