51ac492a4178ab95a6a1ab81381619e029faa814
Seth Hinz <sethhinz@me.com>
2025-06-16 14:11:42 -0700

    hcs slow writer

---
+++ tests/test_hcs_copier.py
@@ -0,0 +1,85 @@
+import random
+import tempfile
+import time
+from pathlib import Path
+from threading import Event, Thread
+
+import iohub
+import iohub.ngff
+import numpy as np
+import tensorstore as ts
+
+
+def slow_write(target: Path):
+    shape = [5, 4, 100, 100, 100]
+    z_chunk = 20
+    archive = ts.open({
+        "driver": "zarr",
+        "kvstore": {
+            "driver": "file",
+            "path": str(target),
+        },
+        "metadata": {
+            "compressor": {
+                "id": "blosc",
+                "cname": "blosclz",
+                "shuffle": 1,
+                "clevel": 3,
+            },
+            "dtype": "<u2",
+            "shape": shape,
+            "chunks": [1, 1, z_chunk, 30, 40],
+        },
+        "create": True,
+        "delete_existing": True,
+    }).result()
+
+    time.sleep(random.uniform(1, 2))  # noqa: S311
+
+    random_slab = np.random.randint(0, 65536, size=(z_chunk, *shape[-2:]), dtype="<u2")
+    for t in range(shape[0]):
+        for v in range(shape[1]):
+            for z_index in range(0, shape[2], z_chunk):
+                archive[t, v, z_index : z_index + z_chunk, ...].write(random_slab).result()
+                time.sleep(random.uniform(0.1, 0.5))  # noqa: S311
+
+
+def slow_write_hcs(target: Path, start_barrier: Event):
+    plate = iohub.open_ome_zarr(
+        target,
+        "hcs",
+        "w",
+        channel_names=["a", "b"],
+    )
+
+    start_barrier.wait()
+
+    targets: list[Path] = []
+    for camera in ("cam1", "cam2"):
+        for color in ("488nm", "561nm", "640nm"):
+            for pos in ("pos1", "pos2", "pos3"):
+                pos: iohub.ngff.Position = plate.create_position(camera, color, pos)
+
+                for image, scale in enumerate([[1] * 5, [1, 1, 0.25, 0.25, 0.25]]):
+                    pos._create_image_meta(str(image), [iohub.ngff.TransformationMeta(type="scale", scale=scale)])
+
+                    targets.append(Path(pos.zattrs.store.path) / Path(pos.zattrs.key).parent / str(image))
+
+    threads = []
+    for target in targets:
+        thread = Thread(target=slow_write, args=(target,))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+
+def test_hcs_copier():
+    root_path = Path(tempfile.mkdtemp())
+    plate_path = root_path / "plate.ome.zarr"
+    print(plate_path)
+    start_barrier = Event()
+    thread = Thread(target=slow_write_hcs, args=(plate_path, start_barrier))
+    thread.start()
+    start_barrier.set()
